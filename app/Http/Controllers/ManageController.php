76d3c79e7531ed32dea1e0b5dbe62d840c98d3f1
Do Le <ledo3012@gmail.com>
2021-04-29 15:39:19 +0700

    First commit

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -10,4 +10,19 @@ class ManageController extends Controller
     public function index() {
         return view('manage.index');
     }
+
+    public function register() {
+        return view('manage.register');
+    }
+
+    public function postRegister(Request $request) {
+        $this->validate($request,
+        [
+
+        ],
+        [
+
+        ]
+        );
+    }
 }
