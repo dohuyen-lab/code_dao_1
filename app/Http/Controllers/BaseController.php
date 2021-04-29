afad41b9d19a27673a895b0a72332146e21df379
Do Le <ledo3012@gmail.com>
2021-04-29 13:09:34 +0700

    Create route and migration

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class BaseController extends Controller
 {
     //
+
+    public function login() {
+        return view('login.login');
+    }
+
+    public function postLogin(Request $request) {
+        // $this->validate();
+    }
 }
